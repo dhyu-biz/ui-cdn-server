ec06549c63a60cffe946c211c2e3ac8daf034df2
YUDAHAN <dbekgks98@gmail.com>
2025-06-24 17:10:40 +0900

    rr

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const App = () => {
-  return <div style={{ fontSize: '20px', color: 'blue' }}>안녕하세요, 아임웹!</div>
+  return <div style={{ fontSize: '20px', color: 'blue' }}>안녕하세요, 아임웹 위젯입니다!</div>
 }
 
 const mountNode = document.getElementById('my-widget')
